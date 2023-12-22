238f5ed55b6374ed8583c1b6ab969ceaf06688f3
fibonacci111 <murzykovz@bk.ru>
2023-12-22 19:46:22 +0300

    awd

--- Assets/Hml/skript/active_anim_E.cs
+++ Assets/Hml/skript/active_anim_E.cs
@@ -4,45 +4,49 @@ using UnityEngine;
 
 public class Desk_animation1 : MonoBehaviour
 {
-  
+
     [SerializeField] Animator animator;
-      public string animation;
-  public string currentAnimation;
-  public bool key=false;
-  private void FixitUpdate()
-  
-{
-  if(Input.GetKeyDown(KeyCode.F))
-{
-  key=true;
-  
-}
-  
+    public string animation;
+    public string currentAnimation;
+    public bool key = false;
+    public bool key1 = false;
+    private void Update()
 
-}
-  void Animationn1(string animation)
-   {
-       if(currentAnimation == animation) return;
-
-
-       animator.Play(animation);
-  
-       currentAnimation = animation;
-	key=false;
- }
-
-    private void OnTriggerStay(Collider other)
     {
-        if(other.CompareTag("Player")&&Input.GetKeyDown(KeyCode.F))
-        { 
-                
+        if (key1 == true&& Input.GetKeyDown(KeyCode.F))
+        {
+            key = true;
 
-              Animationn1(animation);
-}
- else{key=false;}  		 
-            }
-           
-        
-    
-	
+        }
+
+
+    }
+    void Animationn1(string animation)
+    {
+        if (currentAnimation == animation) return;
+
+
+        animator.Play(animation);
+
+        currentAnimation = animation;
+        key = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        key1 = true;
+        if (other.CompareTag("Player") && key == true)
+        {
+
+
+            Animationn1(animation);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    { 
+        key1 = false;
+    }
+
+
+
 }
